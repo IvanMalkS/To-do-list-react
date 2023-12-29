55be2e157737fa88746a434de83eaad73f221a94
Van1s1mys <malkovsin@gmail.com>
2023-12-29 12:09:16 +0500

    test hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import TodoList from './TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const date1 = new Date(2023, 12, 27, 14, 5);
+const date2 = new Date(2024, 1, 27, 14, 5);
+
+const initialData = [
+    {
+        title: "Создать первый проект",
+        desc: "Гойда!",
+        image: '',
+        done: true,
+        createdAt: date1.getTime()
+    },
+    {
+        title: "Написать первый проект",
+        desc: "ПИЗДЕЦ",
+        image: '',
+        done: false,
+        createdAt: date2.getTime()
+    }
+];
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {data:initialData};
+        this.setDone = this.setDone.bind(this);
+    }
+
+    setDone(key) {
+        const deed = this.state.data.find((current) => current.key === key);
+        if (deed)
+            deed.done = true;
+        this.setState((state) => ({}))
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar is-light">
+                    <div className="navbar-brand">
+                        <span className="navbar-item is-uppercase">
+                            Todos
+                        </span>
+                    </div>
+                </nav>
+                <main className="content px-6 mt-6">
+                    <TodoList list={this.data} setDone={this.setDone}/>
+                </main>
+            </div>
+        );
+    }
 }
 
-export default App;
